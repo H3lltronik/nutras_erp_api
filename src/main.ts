ccaa56f8490975c82cdfa697c726d2a829862f12
H3lltronik <esau.egs1@gmail.com>
2023-08-18 09:25:52 -0600

    railway deploy test 1

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(
-    process.env.PORT || 3000,
-    process.env.APP_HTTP_HOST || 'localhost',
-  );
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
