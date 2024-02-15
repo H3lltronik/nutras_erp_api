19dc811bbbe473d9b766f15fa35d8f6773a843fb
Antonio Romano <aromanomacias@gmail.com>
2024-02-14 22:33:35 -0600

    ading more filters to product

--- src/modules/product/filters/products-filters.handler.ts
+++ src/modules/product/filters/products-filters.handler.ts
@@ -7,12 +7,18 @@ import { ProductNameSearchFilter } from './methods/product-name-search.filter';
 import { ProductProviderSearchFilter } from './methods/product-provider-search.filter';
 import { ProductPublishedModeFilter } from './methods/product-published-mode.filter';
 import { ProductTypeFilter } from './methods/product-product-type.filter';
+import { ProductKosherFilter } from './methods/product-kosher.filter';
+import { ProductAllergenFilter } from './methods/product-allergen.filter';
+import { ProductTypesFilter } from './methods/product-types.filter';
 
 export class ProductsFiltersHandler extends BaseFilterHandler<Product> {
   protected filters = {
     nameSearch: new ProductNameSearchFilter(),
     codeSearch: new ProductCodeSearchFilter(),
     providerSearch: new ProductProviderSearchFilter(),
+    kosher: new ProductKosherFilter(),
+    allergen: new ProductAllergenFilter(),
+    productTypes: new ProductTypesFilter(),
     draftMode: new ProductDraftModeFilter(),
     published: new ProductPublishedModeFilter(),
     type: new ProductTypeFilter(),
